714e78c4386f59ece36a1f12d5a45caf1e7e6788
daniel <daniel@xubuntu>
2023-08-12 14:00:15 -0400

    a simple script to play background music.

---
+++ basement/bmusic.lua
@@ -0,0 +1,16 @@
+-- this is just a command to play background music
+-- when I try to use Love2d sample code to play a track of music,
+-- it uses around 20%% of the cpu, while with ffplay here around 5%%,
+-- eventually, when I have time, I will investigate this, but
+-- for the moment, this is good enough
+--
+-- the two percent characters in the lua comment section seem to 
+-- be needed in order to avoid to confuse locco
+
+#!/usr/bin/env lua
+s = 'ffplay -hide_banner -nodisp -loop 0 chant.ogg 2>/dev/null'
+os.execute(s)
+
+-- this script assume that ffplay is installed in your system
+--
+-- chant.ogg is gregorian chant of the Ave Maria
